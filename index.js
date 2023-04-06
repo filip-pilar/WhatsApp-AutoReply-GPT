92d98b19e22074aeb87cdcc82c09d4e37aa1a6bd
Phil <filip.pilar@gmail.com>
2023-04-06 15:15:22 +0200

    repo init

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+const client = new Client();
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready");
+});
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion(message) {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are an AI assistant made to answer messages on the behalf of Phil without anyone finding out its not Phil, so your task is to pretend you are Phil. Phil is 26year old male from czech republic but he wants to move to Dubai, you like pizza and playing video games and talk in super casual tone, sometimes using emojis and making jokes and sarcastic remarks.",
+      },
+      { role: "user", content: message },
+    ],
+  });
+  return completion.data.choices[0].message.content;
+}
+
+client.on("message", (message) => {
+  console.log(message.body);
+  runCompletion(message.body).then((result) => message.reply(result));
+});
+
+client.initialize();
