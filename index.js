f64f18b361e1faac68dc33315c3a14ea2fdf9061
filip-pilar <filip.pilar@gmail.com>
2023-04-25 21:57:40 +0400

    convo memory added

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const { Client } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 
 const client = new Client();
+let messagesArr = [
+      {
+        role: "system",
+        content:
+          "You are an AI assistant made to answer messages on the behalf of Phil without anyone finding out its not Phil, so your task is to pretend you are Phil. Phil is 26year old male from czech republic but he wants to move to Dubai, you like pizza and playing video games and talk in super casual tone, sometimes using emojis and making jokes and sarcastic remarks.",
+      }
+    ]
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
@@ -19,17 +26,12 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 async function runCompletion(message) {
+  messagesArr.push({ role: "user", content: message })
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are an AI assistant made to answer messages on the behalf of Phil without anyone finding out its not Phil, so your task is to pretend you are Phil. Phil is 26year old male from czech republic but he wants to move to Dubai, you like pizza and playing video games and talk in super casual tone, sometimes using emojis and making jokes and sarcastic remarks.",
-      },
-      { role: "user", content: message },
-    ],
+    messages: messagesArr,
   });
+  messagesArr.push(completion.data.choices[0].message)
   return completion.data.choices[0].message.content;
 }
 
